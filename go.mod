9420a6ac35035852694e81ad267b066d85ab3d9d
Angel VI <a.v.iliev13@gmail>
2023-02-16 14:26:41 +0200

    extract gps coords from gpx file

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/AngelVI13/satvar
 
 go 1.18
+
+require github.com/sudhanshuraheja/go-garmin-gpx v1.0.2
+
+require github.com/Zauberstuhl/go-xml v0.0.0-20180122175953-534ffa216723 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"log"
+
+	gpx "github.com/sudhanshuraheja/go-garmin-gpx"
+)
+
+// TODO: visualize gps coordinates on map:
+// https://www.here.com/learn/blog/reverse-geocoding-a-location-using-golang
 func main() {
+	file := "Vilnius100km.gpx"
+	g, err := gpx.ParseFile(file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(len(g.Tracks))
+	if len(g.Tracks) != 1 {
+		log.Fatalf(
+			"currently only 1 track per file is supported: got %d",
+			len(g.Tracks),
+		)
+	}
+
+	track := g.Tracks[0]
+
+	for _, segment := range track.TrackSegments {
+		for _, point := range segment.TrackPoint {
+			log.Println(point.Latitude, point.Longitude, point.Elevation)
+		}
+	}
+
+	log.Println(len(track.TrackSegments))
 }
