83fee7fb82c1d53d0d7308afb713dd7541120c85
Angel VI <a.v.iliev13@gmail>
2023-02-16 15:16:37 +0200

    add km and elevation gain/loss calculation

--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module github.com/AngelVI13/satvar
 
 go 1.18
 
-require github.com/sudhanshuraheja/go-garmin-gpx v1.0.2
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/marcinwyszynski/geopoint v0.0.0-20140302213024-cf2a6f750c5b
+	github.com/sudhanshuraheja/go-garmin-gpx v1.0.2
+)
 
 require github.com/Zauberstuhl/go-xml v0.0.0-20180122175953-534ffa216723 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
 	"log"
+	"net/http"
 
+	geo "github.com/marcinwyszynski/geopoint"
 	gpx "github.com/sudhanshuraheja/go-garmin-gpx"
 )
 
@@ -26,11 +31,103 @@ func main() {
 
 	track := g.Tracks[0]
 
+	var (
+		kms             geo.Kilometres = 0.0
+		geoPoints       []*geo.GeoPoint
+		elevationPoints []float64
+	)
+
 	for _, segment := range track.TrackSegments {
 		for _, point := range segment.TrackPoint {
-			log.Println(point.Latitude, point.Longitude, point.Elevation)
+			elevationPoints = append(elevationPoints, point.Elevation)
+
+			gPoint := geo.NewGeoPoint(geo.Degrees(point.Latitude), geo.Degrees(point.Longitude))
+			geoPoints = append(geoPoints, gPoint)
+
+			if len(geoPoints) <= 1 {
+				continue
+			}
+
+			kms += gPoint.DistanceTo(geoPoints[len(geoPoints)-2], geo.Haversine)
+
 		}
 	}
 
-	log.Println(len(track.TrackSegments))
+	log.Println("Kilometres", kms)
+	log.Println("#Points", len(elevationPoints))
+
+	elevationGain, elevationLoss := calculateElevation(elevationPoints, 60)
+	log.Println("Gain", elevationGain)
+	log.Println("Loss", elevationLoss)
+	// log.Println(elevationPoints)
+
+	/*
+		mux := mux.NewRouter()
+
+		mux.HandleFunc("/", home)
+		mux.HandleFunc("/location/{lat}/{long}", location)
+
+		http.ListenAndServe(":8080", mux)
+	*/
+}
+
+func calculateElevation(points []float64, chunkSize int) (gain, loss float64) {
+	var temp = make([]float64, chunkSize)
+	var avg []float64
+
+	for i := 0; i < len(points); i++ {
+		temp[i%chunkSize] = points[i]
+
+		if i%chunkSize == 0 {
+			avgElev := 0.0
+			for _, elev := range temp {
+				avgElev += elev
+			}
+			avgElev /= float64(chunkSize)
+			avg = append(avg, avgElev)
+		}
+	}
+
+	for i := 1; i < len(avg); i++ {
+		diff := avg[i] - avg[i-1]
+
+		if diff > 0 {
+			gain += diff
+		} else if diff < 0 {
+			loss += (diff * -1.0)
+		}
+	}
+	return gain, loss
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	var templates = template.Must(template.New("geolocate").ParseFiles("getlocation.html"))
+
+	err := templates.ExecuteTemplate(w, "getlocation.html", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	prompt := "Detecting your location. Please click 'Allow' button"
+	w.Write([]byte(prompt))
+
+}
+
+func location(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	lat := vars["lat"]
+	long := vars["long"]
+
+	w.Write([]byte(fmt.Sprintf("Lat is %s \n", lat)))
+
+	w.Write([]byte(fmt.Sprintf("Long is %s \n", long)))
+
+	fmt.Printf("Lat is %s and Long is %s \n", lat, long)
+
+	// if you want to get timezone from latitude and longitude
+	// checkout http://www.geonames.org/export/web-services.html#timezone
+
 }
