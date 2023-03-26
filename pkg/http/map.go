d9f2552851d2ba12320addbc7850234948772d39
Angel VI <a.v.iliev13@gmail>
2023-03-26 17:57:15 +0300

    add missing file

---
+++ pkg/http/map.go
@@ -0,0 +1,22 @@
+package http
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func (s *Server) HandleMap(c *fiber.Ctx) error {
+	longitude := c.Params("long")
+	latitude := c.Params("lat")
+	s.processLocation(longitude, latitude)
+
+	screenWidth := c.Params("sWidth")
+	screenHeight := c.Params("sHeight")
+	s.processScreenSize(screenWidth, screenHeight)
+
+	filename := "Vilnius100km.gpx"
+	svgBytes, err := s.GenerateMap(filename)
+	if err != nil {
+		return c.SendStatus(501)
+	}
+	return c.SendString(string(svgBytes))
+}
